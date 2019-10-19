4cc848a6c86830c130372035439cd9bbb84cdea4
Anish <anishkalpakunj@gmail.com>
2019-10-19 18:37:51 +0545

    Update

--- app/src/main/java/com/example/agilegroupfrontend/YourAuctionDashboardActivity.java
+++ app/src/main/java/com/example/agilegroupfrontend/YourAuctionDashboardActivity.java
@@ -1,13 +1,26 @@
 package com.example.agilegroupfrontend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class YourAuctionDashboardActivity extends AppCompatActivity {
-
+    private CardView cvYourAuctionOnGoingBid, cvYourAuctionSoldOut, cvYourAuctionWinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_auction_dashboard);
+        cvYourAuctionSoldOut = findViewById(R.id.cvYourAuctionSoldOut);
+        cvYourAuctionWinner = findViewById(R.id.cvYourAuctionWinner);
+        cvYourAuctionSoldOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(YourAuctionDashboardActivity.this, YourAuctionSoldOutActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
